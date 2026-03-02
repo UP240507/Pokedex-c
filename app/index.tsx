b7fbe6f3a54d940adf73f964c8aed9e033adb80d
Christopher Rubén Rosales Gómez <up240507@alumnos.upa.edu.mx>
2026-03-02 15:32:22 -0600

    add getPokemons fetch

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,41 @@
+import { useEffect } from "react";
 import { Text, View } from "react-native";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+  useEffect(() => {
+    getPokemons();
+    console.log("Entré en una pantalla");
+  }, []);
+  const getPokemons = async () => {
+    try {
+      const URL = "";
+      const response = await fetch(URL, {
+        method: "GET",
+      });
+
+      if (response.ok) {
+        console.log("Request OK");
+      } else {
+        console.log("Request failed");
+      }
+    } catch (error) {
+      console.log("Error fetching pokemons:", error);
+    }
+
+    const handlePress = () => {
+      console.log("Botón presionado");
+    };
+
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+      </View>
+    );
+  };
 }
